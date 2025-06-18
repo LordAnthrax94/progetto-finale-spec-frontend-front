d2da8094074f592b4f9aeb2ca0c78e39c727e0a9
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-18 18:49:49 +0200

    Fix: aggiunta validazione ricerca senza risultati

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,12 +18,18 @@ export default function Home(){
   const { videogames, searchVideogames, fetchSearchResults } = useContext(GlobalContext);
 
   const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("");
   const [debouncedSearch, setDebouncedSearch] = useState('');
   const debounceRef = useRef(debounce(setDebouncedSearch, 500)); 
 
   const handleSearch = (e) => {
     setSearch(e.target.value)
     debounceRef.current(e.target.value);
+    if( e.target.value.trim() === '') {
+      setMessage("Non è stato trvato nessun gioco con questo nome")
+    }else {
+      setMessage("");
+    }
   } 
 
   useEffect(() => {
@@ -34,17 +40,25 @@ export default function Home(){
 
   const gameShowList = search.trim() === '' ? videogames : searchVideogames 
 
+  const noResults = debouncedSearch.trim() !== '' &&
+    Array.isArray(gameShowList) &&
+    gameShowList.length === 0; 
+
 
   return (
     <div>
       <h1>Esplora un mondo di videogiochi</h1>
-         <input 
+        <div>
+          <input 
             className='search-input'
             type="text"
             placeholder="Cerca un gioco..."
             value={search}
             onChange={handleSearch}            
-         />      
+          />
+            {message && <p className='message'>{message}</p>}
+            {noResults && <p className='message'>Non è stato trovato nessun gioco con questo nome</p>}
+         </div>      
                    
         <div>
           <GameList videogames={Array.isArray(gameShowList) ? gameShowList : []}/>
